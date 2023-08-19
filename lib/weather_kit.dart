a6d34874587364258e3a694c089a94c10ba6dc9c
Konstantin Rolf <konstantin@bytecodes.dev>
2023-08-19 13:41:06 +0200

    Updated parsing

--- lib/weather_kit.dart
+++ lib/weather_kit.dart
@@ -13,11 +13,11 @@ import 'package:weather_kit/src/models/data_set.dart';
 
 class WeatherKit {
   String generateJWT({
-    required String bundleId,
-    required String teamId,
-    required String keyId,
-    required String pem,
-    required Duration expiresIn,
+    required final String bundleId,
+    required final String teamId,
+    required final String keyId,
+    required final String pem,
+    required final Duration expiresIn,
   }) {
     final jwt = JWT(
       {
@@ -43,10 +43,10 @@ class WeatherKit {
 
   /// [country] should be the ISO Alpha-2 country code.
   Future<List<dynamic>> obtainAvailability({
-    required String jwt,
-    required double latitude,
-    required double longitude,
-    required String country,
+    required final String jwt,
+    required final double latitude,
+    required final double longitude,
+    required final String country,
   }) async {
     assert(latitude >= -90 || latitude <= 90);
     assert(latitude >= -180 || latitude <= 180);
@@ -54,54 +54,66 @@ class WeatherKit {
       Uri.parse("$baseUrl/availability/$latitude/$longitude?country=$country"),
       headers: {HttpHeaders.authorizationHeader: jwt},
     );
+    if (!(response.statusCode >= 200 && response.statusCode < 300)) {
+      throw Exception('Failed to obtain availability. Status code: ${response.statusCode}. Response body: ${response.body}');
+    }
     final decode = json.decode(response.body);
+    if (decode is! List) {
+      throw FormatException('Response body is not a list. Response body: ${response.body}');
+    }
 
     return decode;
   }
 
   /// Obtain weather data for the specified location.
   Future<CurrentWeatherData> obtainWeatherData({
-    required String jwt,
-    required String language,
-    required double latitude,
-    required double longitude,
-    required DataSet dataSets,
-    required String timezone,
-    String? countryCode,
-    DateTime? currentAsOf,
-    DateTime? dailyEnd,
-    DateTime? dailyStart,
-    DateTime? hourlyEnd,
-    DateTime? hourlyStart,
+    required final String jwt,
+    required final String language,
+    required final double latitude,
+    required final double longitude,
+    required final DataSet dataSets,
+    required final String timezone,
+    final String? countryCode,
+    final DateTime? currentAsOf,
+    final DateTime? dailyEnd,
+    final DateTime? dailyStart,
+    final DateTime? hourlyEnd,
+    final DateTime? hourlyStart,
   }) async {
-    assert(latitude >= -90 || latitude <= 90);
-    assert(latitude >= -180 || latitude <= 180);
-    String url =
-        "$baseUrl/weather/$language/$latitude/$longitude?dataSets=${dataSets.name}&timezone=$timezone";
+    assert(latitude >= -90.0 || latitude <= 90.0);
+    assert(latitude >= -180.0 || latitude <= 180.0);
+    String url = '$baseUrl/weather/$language/$latitude/$longitude?dataSets=${dataSets.name}&timezone=$timezone';
 
     if (countryCode != null) {
       url = '$url&countryCode=$countryCode';
     }
     if (currentAsOf != null) {
-      url = '$url&currentAsOf=${currentAsOf.toIso8601String()}Z';
+      url = '$url&currentAsOf=${currentAsOf.toUtc().toIso8601String()}';
     }
     if (dailyEnd != null) {
-      url = '$url&dailyEnd=${dailyEnd.toIso8601String()}Z';
+      url = '$url&dailyEnd=${dailyEnd.toUtc().toIso8601String()}';
     }
     if (dailyStart != null) {
-      url = '$url&dailyStart=${dailyStart.toIso8601String()}Z';
+      url = '$url&dailyStart=${dailyStart.toUtc().toIso8601String()}';
     }
     if (hourlyEnd != null) {
-      url = '$url&hourlyEnd=${hourlyEnd.toIso8601String()}Z';
+      url = '$url&hourlyEnd=${hourlyEnd.toUtc().toIso8601String()}';
     }
     if (hourlyStart != null) {
-      url = '$url&hourlyStart=${hourlyStart.toIso8601String()}Z';
+      url = '$url&hourlyStart=${hourlyStart.toUtc().toIso8601String()}';
     }
 
     final response = await http.get(
       Uri.parse(url),
       headers: {HttpHeaders.authorizationHeader: jwt},
     );
-    return CurrentWeatherData.fromJson(response.body);
+    if (!(response.statusCode >= 200 && response.statusCode < 300)) {
+      throw Exception('Failed to obtain availability. Status code: ${response.statusCode}. Response body: ${response.body}');
+    }
+    try {
+      return CurrentWeatherData.fromJson(response.body);
+    } catch (e) {
+      throw FormatException('Failed to parse response body: $e. Response body: ${response.body}');
+    }
   }
 }
